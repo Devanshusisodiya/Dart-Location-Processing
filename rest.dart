87164e57869d1d65da0b8d4fb1159d321254e68e
devanshu <devanshukumar45@gmail.com>
2021-05-24 19:29:02 +0530

    added intensive function body for coords processing

--- rest.dart
+++ rest.dart
@@ -1,11 +1,18 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-void main() {}
+void main() {
+  fetchData("Thor");
+}
 
 void fetchData(name) async {
-  var url = Uri.parse('http://127.0.0.1:8000/test-list-singular/${name}/');
-  var response = await http.get(url);
+  var response = await http
+      .get(Uri.parse('http://127.0.0.1:8000/test-list-singular/${name}/'));
+  var initJson = jsonDecode(response.body);
+
+  var cdInstance = CoordinatesDecode.fromJson(initJson);
+  var requestData = cdInstance.toJson();
+  print(requestData);
 }
 
 class CoordinatesDecode {
